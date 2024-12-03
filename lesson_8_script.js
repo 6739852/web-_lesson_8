7588bd11396f601caf65dc860695cddd23bda07c
neomi <6739852@gmail.com>
2024-12-03 14:12:17 +0200

    etgar

--- lesson_8_script.js
+++ lesson_8_script.js
@@ -1,26 +1,14 @@
-let plus=document.getElementById("plus").value
-let chezka=document.getElementById("chezka").value
-let mul=document.getElementById("mul").value
-let num1=document.getElementById("in1").value
-let num2=document.getElementById("in2").value
-
-
-
-
 function f_plus(){
-   let mul=document.getElementById("mul").value
    let num1=document.getElementById("in1").value
    let num2=document.getElementById("in2").value
    alert(((+num1)+(+num2)))
 }
 function f_mul(){
-    let mul=document.getElementById("mul").value
     let num1=document.getElementById("in1").value
     let num2=document.getElementById("in2").value
     alert(((num1)*(num2)))
 }
 function f_chezka(){
-    let mul=document.getElementById("mul").value
     let num1=document.getElementById("in1").value
     let num2=document.getElementById("in2").value
     alert(((num1)**(num2)))
@@ -31,4 +19,12 @@ function f_save(){
     name=name||"gess";
     sisma=sisma||"1234";
     alert("hello to "+name + "your code is:"+sisma)
+}
+function dec2bin(dec) {
+    return (dec >>> 0).toString(2);
+  }
+function f_plus_binuri(){
+   let num1=document.getElementById("in1").value
+   let num2=document.getElementById("in2").value
+   alert(dec2bin(((+num1)+(+num2))))
 }
